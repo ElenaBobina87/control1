be075eea1114a05f73f2fb94771b469e3ca6207a
elena bobina <putnyk19@mail.ru>
2023-11-01 13:22:30 +0500

    Создали новый массив

--- Program.cs
+++ Program.cs
@@ -4,9 +4,9 @@ string[] strArr = new string[4] { "Hello", "2", "world", ":-)" };
 int count = 0;
 int Size (array)
 {
-    for (int i = 0; i < Array.Length; i++)
+    for (int i = 0; i < array.Length; i++)
     {
-        if (Array[i].Length <= 3)
+        if (array[i].Length <= 3)
         {
             count++;
         }
@@ -14,3 +14,15 @@ int Size (array)
     return count;
 }
 
+string[] Selection(array, arrSelect)
+{
+     int count = 0;
+     for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            arrSelect[count] = array[i];
+            count++;
+        }
+    }
+}
