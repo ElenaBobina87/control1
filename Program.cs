696445b97dcfdb00bef09f113e44d3cb7c471968
elena bobina <putnyk19@mail.ru>
2023-11-01 13:12:16 +0500

    Метод для определения длины нового массива

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Globalization;
+
+string[] strArr = new string[4] { "Hello", "2", "world", ":-)" };
+int count = 0;
+int Size (array)
+{
+    for (int i = 0; i < Array.Length; i++)
+    {
+        if (Array[i].Length <= 3)
+        {
+            count++;
+        }
+    }
+}
